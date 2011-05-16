73f6f94351af71614955b0ced2f3cab6d9090856
Chris Dent <chris.dent@gmail.com>
2011-05-16 15:51:15 +0100

    release 0.9.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.caching'
 DESCRIPTION = 'A wrapping store caching entities in memcached.'
-VERSION = '0.9.8' # duped from tiddlywebplugins/caching.py
+VERSION = '0.9.9' # duped from tiddlywebplugins/caching.py
 
 
 import os
